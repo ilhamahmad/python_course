0701428a0e296a3aa3d9b89549295e69033a81da
İlham Ahmadzada <ahmedzada.ilham@gmail.com>
2025-03-01 16:10:32 +0400

    add test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #  1 tshirt price is 10$
 #  try to get how many tshirt user want to buy
 #  apply 30% discount
-
+# this is just demo
 tshirt_price = 10
 count = int(input('How many tshirt you want buy ? : '))
 
