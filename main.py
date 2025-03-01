486c4837b3e56f12f50ae3a3f6a003dac4e76938
İlham Ahmadzada <ahmedzada.ilham@gmail.com>
2025-03-01 16:07:43 +0400

    first lesson

---
+++ main.py
@@ -0,0 +1,10 @@
+#  1 tshirt price is 10$
+#  try to get how many tshirt user want to buy
+#  apply 30% discount
+
+tshirt_price = 10
+count = int(input('How many tshirt you want buy ? : '))
+
+discount_price = tshirt_price*0.7
+
+print(count*discount_price)
